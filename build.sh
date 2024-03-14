d16b854e9150c7f928c60a825abe6d2093ffdd0a
lionelhost <dplionnel@gmail.com>
2024-03-14 23:40:19 +0000

    fix build script

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ python manage.py collectstatic --no-input
 # Apply any outstanding database migrations
 python manage.py migrate
 
-if [[-z $CREATE_SUPERUSER]];
+if [[$CREATE_SUPERUSER]];
 then python manage.py createsuperuser --no-input
 fi
