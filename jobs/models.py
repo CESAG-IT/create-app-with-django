8d2a8a333c252b62de06cafde5d469ed6957266d
lionelhost <dplionnel@gmail.com>
2024-03-14 19:26:04 +0000

    add models, admin user and template for job

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,46 @@
 from django.db import models
-
 # Create your models here.
+# Un Job est caractérisé par: 
+
+# - id 
+# - reference 
+# - titre 
+# - description 
+# - image 
+# - file 
+# - salaire 
+# - status (open, pending, close)
+# - publish_date 
+# - close_date 
+
+class Job(models.Model):
+
+    STATUS = (
+        ('O', 'OPENED'),
+        ('P', 'PENDING'),
+        ('C','CLOSED')
+    )
+
+    referene = models.CharField(max_length=10)
+    title = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/', default='default.jpg')
+    file = models.FileField(upload_to="uploads/")
+    salaire = models.PositiveIntegerField(default=100000)
+    status = models.CharField(max_length=1, choices=STATUS, default="O")
+    publish_date = models.DateTimeField(auto_now=True)
+    closed_date = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self) -> str:
+        return self.title
+
+    class Meta:
+        db_table = 'jobs'
+        ordering = ['-created_at']
+        verbose_name = 'Job'
+        verbose_name_plural = "jobs"
+
+
--- obonjob/urls.py
+++ obonjob/urls.py
@@ -5,6 +5,9 @@ from jobs.views import home
 from jobs.views import job_list, create_job, show_job 
 from jobs.views import update_job, delete_job
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
@@ -13,4 +16,4 @@ urlpatterns = [
     path('show-job/<int:pk>', show_job),
     path('update-job/<int:pk>', update_job),
     path('delete-job/<int:pk>', delete_job)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
