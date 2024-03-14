04389e0a5bdcd408d3f6ea6238c25486fe060ed9
lionelhost <dplionnel@gmail.com>
2024-03-14 23:07:38 +0000

    change allowed app

--- obonjob/settings.py
+++ obonjob/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = env('APP_ALLOWED_HOST')
+ALLOWED_HOSTS = env('APP_ALLOWED_HOST').split(" ")
 
 
 # Application definition
